673b048514a3a47378a7ceb4f953583ffb6601a2
Matthew Martinez <matt.martinez2120@gmail.com>
2022-12-14 16:09:46 -0600

    first commit

---
+++ src/Components/Dropdown/DropDown.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Users } from "../users";
+import "../Dropdown/DropDown.css";
+
+const DropDown = () => {
+    const [query, setQuery] = useState("");
+
+    return (
+    <div className="app">
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+      <ul className="list">
+        {Users.filter((asd) =>
+          asd.first_name.toLowerCase().includes(query)
+        ).map((user) => (
+          <li className="listItem" key={user.id}>
+            {user.first_name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DropDown;
